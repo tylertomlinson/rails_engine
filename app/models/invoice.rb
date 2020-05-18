56e4aa0c9720a8446ef3d73f457bf9984066c618
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-18 13:19:42 -0600

    Add Invoice with test

---
+++ app/models/invoice.rb
@@ -0,0 +1,9 @@
+class Invoice < ApplicationRecord
+  validates :status, presence: true
+
+  belongs_to :customer
+  belongs_to :merchant
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items
+  has_many :transactions, dependent: :destroy
+end
