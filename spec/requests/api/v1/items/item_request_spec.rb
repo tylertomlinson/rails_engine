4eacb193970b4843778f5ec1b3d19ac771099e2d
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-19 19:52:13 -0600

    Refactor test to be in one file

--- spec/requests/api/v1/items/item_request_spec.rb
+++ spec/requests/api/v1/items/item_request_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'single merchant items page' do
-  it 'shows all items for a specific merchant' do
-    merchant = create(:merchant)
-    item1 = create(:item, merchant_id: merchant.id)
+  before(:each) do
+    @merchant = create(:merchant)
+
+    create_list(:item, 7)
+
     get '/api/v1/items'
 
+    @items = JSON.parse(response.body)
+  end
+
+  it 'can get list of items' do
     expect(response).to be_successful
-
-    items = JSON.parse(response.body)
-
-    expect(items.class).to eq(Hash)
-    expect(items['data'].length).to eq(3)
-    expect(items['data'].first['attributes']['name']).to eq(item1.name)
-    expect(items['data'].first['attributes']['merchant_id']).to eq(merchant.id)
+    expect(@items['data'].count).to eq(7)
+    expect(@items.class).to eq(Hash)
   end
 end
+
+# expect(items['data'].first['attributes']['name']).to eq(item1.name)
+# expect(items['data'].first['attributes']['merchant_id']).to eq(merchant.id)
