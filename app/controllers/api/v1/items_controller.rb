3be441164cbe62ea6c18e498db9e5865574c64ac
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-20 12:57:30 -0600

    Item endpoints testing complete

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,25 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    merchant = Merchant.find(item_params[:merchant_id])
+    render json: ItemSerializer.new(merchant.items.create(item_params))
+  end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    Item.delete(params[:id])
+    render json: ItemSerializer.new(item)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
