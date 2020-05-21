d5648855b65594843269039a58e4527936d02e22
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-05-20 22:13:51 -0600

    Add name seach methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,12 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.search_by_name(name)
+    where("LOWER(name) LIKE ?", "%#{name.downcase}%").first
+  end
+
+  def self.search_all_by_name(name)
+    where("LOWER(name) LIKE ?", "%#{name.downcase}%")
+  end
 end
